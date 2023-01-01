941f4b543205823f700aed4293c7d6c2d73c8209
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-01-02 00:43:26 +0530

    Create Bicycle world.cmd

---
+++ Bicycle world.cmd
@@ -0,0 +1,39 @@
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ ls
+brands.txt  freight  mountain  racing
+$ cd freight
+$ ls
+messenger  porteur
+$ cd porteur
+$ cd ../..
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ ls
+brands.txt  freight  mountain  racing
+$ cd mountain/downhill
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii/mountain/downhill
+$ touch dirt.txt
+$ touch mud.txt
+$ ls
+dirt.txt  heavyweight  lightweight  mud.txt
+$ mkdir safety
+$ cd home/ccuser/workspace/bicycle-world-ii
+bash: cd: home/ccuser/workspace/bicycle-world-ii: No such file or directory
+$ cd /home/ccuser/workspace/bicycle-world-ii
+$ ls
+brands.txt  freight  mountain  racing
+$ mkdir bmx
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ cd bmx/tricks.txt
+bash: cd: bmx/tricks.txt: No such file or directory
+$ touch bmx/tricks.txt
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ ls
+bmx  brands.txt  freight  mountain  racing
+$ cd bmx
+$ ls
+tricks.txt
