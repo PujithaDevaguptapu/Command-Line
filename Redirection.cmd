3a9535e4d5120c1e90064241827ede4c6a02547a
PujithaDevaguptapu <94973025+PujithaDevaguptapu@users.noreply.github.com>
2023-01-08 22:32:51 +0530

    Create Redirection.cmd

---
+++ Redirection.cmd
@@ -0,0 +1,21 @@
+$ pwd
+/home/ccuser/workspace/geography
+$ ls
+continents.txt  forests.txt   hello.txt    lakes.txt      oceans.txt  volcanoes.txt
+deserts.txt     glaciers.txt  islands.txt  mountains.txt  rivers.txt
+$ cat continents.txt
+Africa
+Antarctica
+Asia
+Australia
+Europe
+North America
+South America
+
+$ cat oceans.txt  > continents.txt
+$ cat continents.txt
+Arctic Ocean
+Atlantic Ocean
+Indian Ocean
+Pacific Ocean
+Southern Ocean
